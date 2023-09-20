c7ca0130c3070256ba2bd08b53266e9769ebaf80
Besitzer <mousllimoumn@gmail.com>
2023-09-20 11:50:01 +0200

    init

---
+++ app/src/main/java/com/example/habbybirthday/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.habbybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.habbybirthday.ui.theme.HabbyBirthdayTheme
+import androidx.compose.ui.unit.sp
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HabbyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(message = "Happy Birthday James!", from ="From Emma")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun  GreetingText(message: String, from:String, modifier: Modifier = Modifier) {
+   Column(
+       verticalArrangement = Arrangement.Center,
+       modifier = modifier.padding(8.dp)
+   ){
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+
+
+            )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(aligment = Alignment.End)
+
+
+                   
+
+        )
+    }
+}
+
+
+
+@Preview(
+    showBackground = true,
+     showSystemUi = true,
+    name = "Meine priwiu"
+
+)
+
+@Composable
+fun BirthdayCardPreview() {
+    HabbyBirthdayTheme {
+        GreetingText(message = "Happy Birthday James!", from ="From Emma")
+
+    }
+}
